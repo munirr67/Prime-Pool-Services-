9565ad88ada2dbf577ce1956cac756341d98d624
munirr67 <munirahmd27@gmail.com>
2026-03-04 19:11:38 +0500

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -12,7 +12,7 @@ const services = [
   {
     title: "Commercial Pool Services",
     desc: "High-traffic pools require expert care to meet safety standards and health code requirements. We specialize in commercial pool service designed to keep facilities compliant, reliable, and guest-ready during peak usage.",
-    img: "https://unsplash.com/photos/stainless-steel-railings-on-swimming-pool-JhcQHSBMask"
+    img: "https://images.unsplash.com/photo-1621245700087-a8617f91cdbe?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
   },
   {
     title: "Pool Maintenance",
